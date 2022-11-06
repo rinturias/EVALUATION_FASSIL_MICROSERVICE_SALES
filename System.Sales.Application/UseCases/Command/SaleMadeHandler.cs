2f38a16b4aa23b118fb91de247be80f656d526bb
Ronal Inturias <ronal.95.03@gmail.com>
2022-11-05 21:32:24 -0400

    se creo los endpoint de creacion de venta y entrega de la venta

---
+++ System.Sales.Application/UseCases/Command/SaleMadeHandler.cs
@@ -0,0 +1,63 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Sales.Application.Dto;
+using System.Sales.Domain.Factories;
+using System.Sales.Domain.Interfaces;
+using System.Sales.Domain.Models.sale;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Sales.Application.UseCases.Command
+{
+    public class SaleMadeHandler:IRequestHandler<SaleMadeCommand, ResulService>
+    {
+        const int active_registry= 0;
+        public readonly ISaleFactory _ISaleFactory;
+        public readonly IUnitOfWork _unitOfWork;
+        public readonly ISaleRepository _ISaleRepository;
+
+
+        public SaleMadeHandler(ISaleFactory iSaleFactory, IUnitOfWork unitOfWork, ISaleRepository iSaleRepository)
+        {
+          
+            _ISaleFactory = iSaleFactory;
+            _unitOfWork = unitOfWork;
+            _ISaleRepository = iSaleRepository;
+        }
+
+        public async Task<ResulService> Handle(SaleMadeCommand request, CancellationToken cancellationToken)
+        {
+
+            try
+            {
+
+                Sale objSale = _ISaleFactory.Create(active_registry,request.Detail.note,request.Detail.clienteId,request.Detail.userId,request.Detail.discount);
+
+                foreach (var item in request.Detail.listDetailsSaleDto)
+                {
+                    objSale.agregarItem(item.productId, objSale.Id, item.price,item.quantity);
+                }
+
+                 objSale.consolidateSale();
+
+                 await _ISaleRepository.CreateAsync(objSale);
+
+                await _unitOfWork.Commit();
+
+                return new ResulService { data = objSale.Id, messaje = "se realizo una venta" };
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return new ResulService { success = false, error = ex.Message, codError = "COD501", messaje = "ERROR crear la venta" };
+
+            }
+
+
+        }
+
+
+    }
+}
---
+++ System.Sales.Infrastructure/Extensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using System.Sales.Infrastructure.EF.Context;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Sales.Domain.Interfaces;
+using System.Sales.Infrastructure.Repositories;
+using System.Sales.Infrastructure.EF;
+using System.Sales.Application;
+using MassTransit;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace System.Sales.Infrastructure
+{
+    public static class Extensions
+    {
+
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+         
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            var connectionString = configuration.GetConnectionString("SalesDbConnectionString");
+
+            services.AddDbContext<ReadDbContext>(context => context.UseSqlServer(connectionString));
+            services.AddDbContext<WriteDbContext>(context => context.UseSqlServer(connectionString));
+
+          
+
+            services.AddScoped<ISaleRepository, SaleRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            //AddRabbitMq(services, configuration);
+
+            return services;
+        }
+
+    }
+}
